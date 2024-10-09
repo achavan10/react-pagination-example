03628d7fb4c006f23818697d5b5db2226cced601
achavan10 <akshayechavan11@gmail.com>
2024-10-09 11:26:25 +0530

    Added code for pagination for react component using custom hook

---
+++ src/component/PaginatedComp.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import usePaginatedData from '../hooks/usePaginatedData';
+
+const PaginatedComp = () => {
+  const url = 'https://jsonplaceholder.typicode.com/posts';
+  // custom hook which will handle data fetching, error, loading for this component
+  const {
+    data,
+    loading,
+    error,
+    currentPage,
+    totalPages,
+    handleNextPage,
+    handlePrevPage,
+  } = usePaginatedData(url, 10);
+
+  return (
+    <div>
+      <h1>Paginated Posts</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {!loading && !error && (
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              {item.id}. {item.title}
+            </li>
+          ))}
+        </ul>
+      )}
+
+      <div className="pagination-container">
+        <button onClick={handlePrevPage} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <span style={{ margin: "0 2rem" }}>
+          Page {currentPage} of {totalPages}
+        </span>
+        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PaginatedComp;
